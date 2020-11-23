caa32cb38ec01f6a8cae3c29fc39e9ff7939774d
Akatsuka0426 <iwatatenno99@yahoo.co.jp>
2020-11-23 10:08:57 +0900

    add members

---
+++ members/furuchi011108.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width">
+        <title>所属会員プロフィール - Uniant</title>
+        <meta name="twitter:card" content="summary">
+        <meta name="twitter:site" content="@Uniant">
+        <meta property="og:url" content="https://uniant.net/members/furuchi011108.php">
+        <meta property="og:title" content="所属会員プロフィール - Uniant">
+        <meta property="og:description" content="Uniant に所属する会員の情報をご覧になれます。">
+        <meta property="og:image" content="uniantlogo.png">
+        <link href="/style.css" rel="stylesheet" type="text/css">
+        <link href="./style.css" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="decoration.css">
+        <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
+        <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.5.0.js"></script>
+    </head>
+    <body>
+    <div class="header">
+        <?php include('../header.php') ?>
+    </div>
+        <div class="content">
+            <div class="membersname">
+                <span class="name">古地 明宏</span><br>
+                <span class="nameeng">Furuchi Akihiro</span><br><br>
+                <span class="mail">furuchi011108@uniant.net</span>
+            </div>
+            <div class="about">
+                <div class="position">
+                    <span class="title2">役　職</span><br>
+                    <div class="positioncontent">
+                        <span class="chairman">会　長</span>
+                        <span class="exective">幹　部</span>
+                        <span class="development">開発部</span>
+                        <span class="publicrelations">広報部</span>
+                        <span class="planning">企画部</span>
+                        <span class="accouting">経理部</span>
+                    </div>
+                </div>
+                <div class="sns">
+                    <span class="title2">ソーシャルアカウント</span><br>
+                    <div class="snscontent">
+                    <span class="twitter">Twitter: @Komeiji_Fran</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer">
+            <?php include('../footer.php') ?>
+        </div>
+    </body>
+</html>
