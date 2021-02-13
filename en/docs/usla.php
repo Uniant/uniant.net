3c78dc2a65bcc163852c4c123496d1158f1f333f
Akatsuka0426 <iwatatenno99@yahoo.co.jp>
2021-02-13 13:33:04 +0900

    微修正

--- en/docs/usla.php
+++ en/docs/usla.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width">
         <meta name="robots" content="noindex">
-        <title>Uniant ソフトウェアライセンス契約（USLA） - Uniant</title>
+        <title>Uniant Software License Agreement - Uniant</title>
         <meta name="twitter:card" content="summary">
         <meta name="twitter:site" content="@Uniant">
         <meta property="og:url" content="https://uniant.net/docs/usla.php">
-        <meta property="og:title" content="Uniant ソフトウェアライセンス契約（USLA） - Uniant">
-        <meta property="og:description" content="Uniant ソフトウェアライセンス契約（USLA）をご覧になれます。">
+        <meta property="og:title" content="Uniant Software License Agreement - Uniant">
+        <meta property="og:description" content="You can see the Uniant Software License Agreement.">
         <meta property="og:image" content="uniantlogo.png">
         <link href="../style.css" rel="stylesheet" type="text/css" />
         <link rel="stylesheet" href="../decoration.css">
@@ -23,55 +23,55 @@
         </div>
         <div class="content">
             <div class="rulestitle">
-                <span>Uniant ソフトウェアライセンス契約（USLA）</span>
+                <span>Uniant Software License Agreement</span>
             </div>
             <div class="rulesdate">
-                <span>令和 2 年 9 月 13 日　制定</span>
+                <span>Decided on September 13th, 2020.</span>
             </div>
             <div class="rulesmain">
                 <div class="articlemain">
-                    <span>Uniant とお客様（以下、ユーザーといいます。）とは、以下の通りに本 Uniant ソフトウェアライセンス契約（以下、本ライセンス契約といいます。）を締結します。</span>
+                    <span>Uniant and the customer (hereinafter referred to as the user) conclude this Uniant Software License Agreement (hereinafter referred to as the License Agreement) as follows.</span>
                 </div>
                 <div class="article" id="1a">
-                    <span>第 1 条　使用許諾</span>
+                    <span>Article 1 Lisence</span>
                 </div>
                 <div class="articlemain">
-                    <span id="1a1">第 1 項　本ライセンス契約は次に指定するアプリケーション、プログラム等（以下、本ソフトウェアといいます。）に適用されます。</span><br>
+                    <span id="1a1">Clause 1　This license agreement applies to the applications, programs, etc. (hereinafter referred to as "Software") specified below.</span><br>
                     <div class="ruleslist">
-                        <span id="1a1-1">・本ソフトウェアの以降のバージョン</span><br>
-                        <span id="1a1-2">・更新プログラム</span><br>
-                        <span id="1a1-3">・追加コンポーネント</span><br>
-                        <span id="1a1-4">・サポートサービス</span><br>
+                        <span id="1a1-1">- Later versions of this software.</span><br>
+                        <span id="1a1-2">- Update programs.</span><br>
+                        <span id="1a1-3">- Additional components</span><br>
+                        <span id="1a1-4">- Support service</span><br>
                     </div>
-                    <span id="1a2">第 2 項　Uniant はユーザーに対し、 Uniant が提供する本ソフトウェアを使用することを承諾します。</span><br>
-                    <span id="1a3">第 3 項　ユーザーは、本ソフトウェアの使用権を第三者に譲渡してはいけません。</span>
+                    <span id="1a2">Clause 2 Uniant consents to you to use users the Software provided by Uniant.</span><br>
+                    <span id="1a3">Clause 3 The user must not transfer the right to use this software to a third party.</span>
                 </div>
                 <div class="article" id="2a">
-                    <span>第 2 条　保証</span>
+                    <span>Article 2 Guarantee</span>
                 </div>
                 <div class="articlemain">
-                    <span id="2a1">第 1 項　Uniant は、ユーザーが <a href="#4a" class="animebuttonlite">第 4 条</a> に指定する行為を実施していないときに限り、稼働環境において本ソフトウェアが動作することを保証します。</span><br>
-                    <span id="2a2">第 2 項　Uniant は、ユーザーが本ソフトウェアを使用する際に生じた不具合について、一切の責任を負わないものとします。</span>
+                    <span id="2a1">Clause 1 Uniant guarantees that the Software will work in the operating environment only if the user has not performed the actions specified in <a href="#4a" class="animebuttonlite">Article 4</a>.</span><br>
+                    <span id="2a2">Clause 2 Uniant shall not be liable for any problems that may occur when the user uses this software.</span>
                 </div>
                 <div class="article">
-                    <span id="3a">第 3 条　所有権</span>
+                    <span id="3a">Article 3 Ownership</span>
                 </div>
                 <div class="articlemain">
-                    <span id="3a1">第 1 項　本ソフトウェアおよびその記憶媒体、マニュアル、資料等の一切の所有権は、 Uniant に帰属します。</span>
+                    <span id="3a1">Clause 1 All ownership of this software and its storage media, manuals, materials, etc. belongs to Uniant.</span>
                 </div>
                 <div class="article">
-                    <span id="4a">第 4 条　禁止事項</span>
+                    <span id="4a">Article 4 Prohibitions</span>
                 </div>
                 <div class="articlemain">
-                    <span id="4a1">第 1 項　ユーザーは、以下の行為をしてはいけません。</span>
+                    <span id="4a1">Clause 1 The user must not do the following.</span>
                     <div class="ruleslist">
-                        <span id="4a1-1">・本ソフトウェア、マニュアル、仕様書、資料等の複製</span><br>
-                        <span id="4a1-2">・本ソフトウェアに関する技術上の秘密の漏洩</span><br>
-                        <span id="4a1-3">・本ソフトウェアの改造、変更</span><br>
-                        <span id="4a1-4">・本ソフトウェアのリバースエンジニアリング、逆コンパイルまたは逆アセンブル</span><br>
+                        <span id="4a1-1">- Reproduction of this software, manuals, specifications, materials, etc.</span><br>
+                        <span id="4a1-2">- Leakage of technical secrets related to this software.</span><br>
+                        <span id="4a1-3">- Modification or change of this software.</span><br>
+                        <span id="4a1-4">- Reverse engineering, decompiling or disassembling the software</span><br>
                     </div>
-                    <span id="4a2">第 2 項　前項に違反し、お客様が本ソフトウェアを改造した場合、当該部分に係る著作権、特許権等、一切の法的権利は Uniant に帰属するものとします。</span><br>
-                    <span id="4a3">第 3 項　ユーザーは、前項の改造等の使用に関し、著作者人格権を行使しません。</span>
+                    <span id="4a2">Clause 2 If the customer modifies this software in violation of the preceding paragraph, all legal rights such as copyrights and patent rights related to the relevant part shall belong to Uniant.</span><br>
+                    <span id="4a3">Clause 3 The user does not exercise the moral rights of the author regarding the use of the modifications, etc. in the preceding paragraph.</span>
                 </div>
                 <div class="article" id="5a">
                     <span>第 5 条　本ソフトウェアの譲渡及び転貸の禁止</span>
