6b8ee7b4bb1f4a62813ed73f39b647394505eb4d
Akatsuka0426 <iwatatenno99@yahoo.co.jp>
2021-04-03 11:10:02 +0900

    リスト制作

---
+++ members/index.php
@@ -0,0 +1,211 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width">
+        <title>所属会員プロフィール - Uniant</title>
+        <meta name="twitter:card" content="summary">
+        <meta name="twitter:site" content="@UniantJapan">
+        <meta property="og:url" content="https://uniant.net/members/">
+        <meta property="og:title" content="所属会員プロフィール - Uniant">
+        <meta property="og:description" content="Uniant に所属する会員の情報をご覧になれます。">
+        <meta property="og:image" content="https://uniant.net/uniantlogoblack.png">
+        <link href="../style.css" rel="stylesheet" type="text/css">
+        <link href="./style.css" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="../decoration.css">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300&family=Open+Sans:wght@300&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <style>
+            .memberabout {
+                height: 45px;
+                display: flex;
+                justify-content: space-between;
+            }
+            .member span {
+                font-size: 30px;
+            }
+            .memberposition {
+                float: left;
+                margin-top: 9px;
+                margin-bottom: 9px;
+            }
+        </style>
+        <div class="header">
+            <?php include('../header.php') ?>
+        </div>
+        <div class="membersname">
+            <div class="membersnametext">
+                <span class="name">所属会員プロフィール</span><br>
+                <span class="nameeng">Members Profile</span><br><br>
+                <span class="mail">プロフィールをご覧になりたい会員の名前をクリックしてください。</span>
+            </div>
+        </div>
+        <div class="content">
+            <hr class="partition">
+            <div class="memberabout">
+                <div class="member">
+                    <span><a href="./furuchi011108">古地明宏</a></span>
+                </div>
+                <div class="memberposition">
+                    <span class="chairman">会　長</span>
+                    <span class="exective">幹　部</span>
+                </div>
+            </div>
+            <hr class="partition">
+            <div class="memberabout">
+                <div class="member">
+                    <span><a href="./akatsuka010426">赤塚弘和</a></span>
+                </div>
+                <div class="memberposition">
+                    <span class="exective">幹　部</span>
+                </div>
+            </div>
+            <hr class="partition">
+            <div class="memberabout">
+                <div class="member">
+                    <span><a href="./hiragi011031">柊翠々</a></span>
+                </div>
+            </div>
+            <hr class="partition">
+            <div class="memberabout">
+                <div class="member">
+                    <span><a href="./takigawa010318">滝川秀吉</a></span>
+                </div>
+            </div>
+            <hr class="partition">
+            <div class="memberabout">
+                <div class="member">
+                    <span><a href="./shashiki010528">射敷文</a></span>
+                </div>
+            </div>
+            <hr class="partition">
+            <div class="memberabout">
+                <div class="member">
+                    <span><a href="./suzuki010914">鈴木崇央</a></span>
+                </div>
+            </div>
+            <hr class="partition">
+            <div class="memberabout">
+                <div class="member">
+                    <span><a href="./yamazaki010203">山崎熊雄</a></span>
+                </div>
+            </div>
+            <hr class="partition">
+            <div class="memberabout">
+                <div class="member">
+                    <span><a href="./hakata0110002">博多太郎</a></span>
+                </div>
+                <div class="memberposition">
+                    <span class="exective">幹　部</span>
+                </div>
+            </div>
+            <hr class="partition">
+            <div class="memberabout">
+                <div class="member">
+                    <span><a href="./horita011119">堀田直人</a></span>
+                </div>
+                <div class="memberposition">
+                    <span class="exective">幹　部</span>
+                </div>
+            </div>
+            <hr class="partition">
+            <div class="memberabout">
+                <div class="member">
+                    <span><a href="./kokoro010113">心陽菜</a></span>
+                </div>
+                <div class="memberposition">
+                    <span class="exective">幹　部</span>
+                </div>
+            </div>
+            <hr class="partition">
+            <div class="memberabout">
+                <div class="member">
+                    <span><a href="./maitani011229">舞谷咲良</a></span>
+                </div>
+                <div class="memberposition">
+                    <span class="exective">幹　部</span>
+                </div>
+            </div>
+            <hr class="partition">
+            <div class="memberabout">
+                <div class="member">
+                    <span><a href="./kasuga010827">春日瞬</a></span>
+                </div>
+            </div>
+            <hr class="partition">
+            <div class="memberabout">
+                <div class="member">
+                    <span><a href="./saito010513">斎藤遥人</a></span>
+                </div>
+            </div>
+            <hr class="partition">
+            <div class="memberabout">
+                <div class="member">
+                    <span><a href="./oshima010413">大島あおい</a></span>
+                </div>
+                <div class="memberposition">
+                    <span class="exective">幹　部</span>
+                </div>
+            </div>
+            <hr class="partition">
+            <div class="memberabout">
+                <div class="member">
+                    <span><a href="./aihara010329">相原健二</a></span>
+                </div>
+            </div>
+            <hr class="partition">
+            <div class="memberabout">
+                <div class="member">
+                    <span><a href="./amashiro010416">天白月見</a></span>
+                </div>
+                <div class="memberposition">
+                    <span class="exective">幹　部</span>
+                </div>
+            </div>
+            <hr class="partition">
+            <div class="memberabout">
+                <div class="member">
+                    <span><a href="./nishiura010828">西浦恭太</a></span>
+                </div>
+                <div class="memberposition">
+                    <span class="exective">幹　部</span>
+                </div>
+            </div>
+            <hr class="partition">
+            <div class="memberabout">
+                <div class="member">
+                    <span><a href="./shisui010831">酒々井乙彦</a></span>
+                </div>
+            </div>
+            <hr class="partition">
+            <div class="memberabout">
+                <div class="member">
+                    <span><a href="./takasugi010306">高杉理史</a></span>
+                </div>
+            </div>
+            <hr class="partition">
+            <div class="memberabout">
+                <div class="member">
+                    <span><a href="./nakatomi010806">中臣信太朗</a></span>
+                </div>
+            </div>
+            <hr class="partition">
+            <div class="memberabout">
+                <div class="member">
+                    <span><a href="./mikuniya011201">三国谷要背位</a></span>
+                </div>
+            </div>
+            <hr class="partition">
+            <div class="memberabout">
+                <div class="member">
+                    <span><a href="./izushima011103">伊豆島武史</a></span>
+                </div>
+            </div>
+        </div>
+        <div class="footer">
+            <?php include('../footer.php') ?>
+        </div>
+    </body>
+</html>
